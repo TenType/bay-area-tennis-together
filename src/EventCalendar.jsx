c3ea37d351ff4e3a604c5d4230e689c80bed6431
JaydenSz <89436523+JaydenSz@users.noreply.github.com>
2024-08-07 10:32:05 -0700

    calander

--- src/EventCalendar.jsx
+++ src/EventCalendar.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 function EventCalendar() {
   return (
-    <div>
-      <h3>Upcoming Events</h3>
-      <p>Interactive calendar coming soon...</p>
+    <div className="event-calendar">
+      <iframe 
+        src="https://calendar.google.com/calendar/embed?src=jacksonhtoo07%40gmail.com&ctz=America%2FLos_Angeles"
+        style={{ border: 0 }} 
+        width="800" 
+        height="600" 
+        frame-border="0" 
+      ></iframe>
     </div>
   );
 }
