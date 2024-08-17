91a5e34c6c062a89cb7ffb86e1e12f7bf662efb9
DanielWantsToGoToIvies <danielguo1098@gmail.com>
2024-08-17 02:58:42 -0700

    calendar looks a lil better

--- src/EventCalendar.jsx
+++ src/EventCalendar.jsx
@@ -2,7 +2,7 @@ function EventCalendar() {
   return (
     <div className="event-calendar">
       <iframe
-        src="https://calendar.google.com/calendar/embed?src=395d8f3924c90c75cd059733b497e7a65be0bf2b5333e54841115c5fa5b75128%40group.calendar.google.com&ctz=America%2FLos_Angeles"
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FLos_Angeles&bgcolor=%237CB342&showCalendars=0&showTabs=0&showPrint=0&src=Mzk1ZDhmMzkyNGM5MGM3NWNkMDU5NzMzYjQ5N2U3YTY1YmUwYmYyYjUzMzNlNTQ4NDExMTVjNWZhNWI3NTEyOEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%23E67C73"
         style={{ border: 0 }}
         width="800"
         height="600"
