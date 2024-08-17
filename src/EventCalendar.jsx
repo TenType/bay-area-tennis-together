0034842b92278c19129008483c4aced4a7f64092
DanielWantsToGoToIvies <danielguo1098@gmail.com>
2024-08-17 02:40:34 -0700

    calendar change

--- src/EventCalendar.jsx
+++ src/EventCalendar.jsx
@@ -2,10 +2,12 @@ function EventCalendar() {
   return (
     <div className="event-calendar">
       <iframe
-        src="https://calendar.google.com/calendar/embed?src=jacksonhtoo07%40gmail.com&ctz=America%2FLos_Angeles"
+        src="https://calendar.google.com/calendar/embed?src=395d8f3924c90c75cd059733b497e7a65be0bf2b5333e54841115c5fa5b75128%40group.calendar.google.com&ctz=America%2FLos_Angeles"
         style={{ border: 0 }}
         width="800"
         height="600"
+        frameBorder="0"
+        scrolling="no"
       ></iframe>
     </div>
   );
